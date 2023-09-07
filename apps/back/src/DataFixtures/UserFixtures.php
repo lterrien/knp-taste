2cbda3ce0da93b24e5b4b4a44a7cbbf9ca28187b
Lea Terrien <lea.terrien@knplabs.com>
2023-09-07 15:42:03 +0200

    refactor: update user fixtures to use UserFactory

--- apps/back/src/DataFixtures/UserFixtures.php
+++ apps/back/src/DataFixtures/UserFixtures.php
@@ -2,12 +2,22 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\User;
+use App\Enum\UserRole;
+use App\Service\Factory\UserFactory;
+use DateTimeImmutable;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
 class UserFixtures extends Fixture
 {
+    const IS_ADMIN = 'isAdmin';
+    const VIEWS_COUNT = 'viewsCount';
+    const LAST_COURSE_VIEW_DATE = 'lastCourseViewDate';
+
+    public function __construct(private readonly UserFactory $userFactory)
+    {
+    }
+
     public function load(ObjectManager $manager): void
     {
         /* Get necessary data to create users (ex: is admin) */
@@ -16,23 +26,21 @@ class UserFixtures extends Fixture
 
         /* Foreach user, create user data and persist it */
         foreach ($courseUserData as $userData) {
-            $user = new User();
 
-            // User data related to security and profile
-            $user->setUsername('user' . $i);
-            $user->setEmail('user' . $i . '@knplabs.com');
-            $user->setHashedPassword('password' . $i);
+            $user = $this->userFactory->getUserInstance(
+                'user' . $i . '@knplabs.com',
+                'password' . $i,
+                'user' . $i
+            );
 
-            // Define roles depending on if user is admin
-            if($userData[0]) {
-                $user->setRoles(['ROLE_USER', 'ROLE_ADMIN']);
-            } else {
-                $user->setRoles(['ROLE_USER']);
+            // Add admin role if user is admin
+            if($userData[self::IS_ADMIN]) {
+                $user->addRole(UserRole::RoleAdmin);
             }
 
             // User data related to courses
-            $user->setViewsCount($userData[1]);
-            $user->setLastView($userData[2]);
+            $user->setViewsCount($userData[self::VIEWS_COUNT]);
+            $user->setLastCourseViewDate($userData[self::LAST_COURSE_VIEW_DATE]);
 
             $manager->persist($user);
             $i++;
@@ -42,18 +50,37 @@ class UserFixtures extends Fixture
     }
 
     /**
-     * Get necessary data to create CourseUser instances
-     * Data in array: isAdmin, viewsCount, lastView
+     * Get necessary data to create User instances
      * @return array
      */
     private static function getCourseUserData(): array
     {
         return [
-            [true, 3, strtotime('yesterday')],
-            [true, 15, strtotime('-10 days')],
-            [false, 2, strtotime('today')],
-            [false, 12, strtotime('-5 days')],
-            [false, 10, strtotime('today')]
+            [
+                self::IS_ADMIN => true,
+                self::VIEWS_COUNT => 3,
+                self::LAST_COURSE_VIEW_DATE => new DateTimeImmutable('yesterday')
+            ],
+            [
+                self::IS_ADMIN  => true,
+                self::VIEWS_COUNT => 15,
+                self::LAST_COURSE_VIEW_DATE => new DateTimeImmutable('-10 days')
+            ],
+            [
+                self::IS_ADMIN  => false,
+                self::VIEWS_COUNT => 2,
+                self::LAST_COURSE_VIEW_DATE => new DateTimeImmutable('today')
+            ],
+            [
+                self::IS_ADMIN  => false,
+                self::VIEWS_COUNT => 12,
+                self::LAST_COURSE_VIEW_DATE => new DateTimeImmutable('-5 days')
+            ],
+            [
+                self::IS_ADMIN  => false,
+                self::VIEWS_COUNT => 10,
+                self::LAST_COURSE_VIEW_DATE => new DateTimeImmutable('today')
+            ]
         ];
     }
 }
